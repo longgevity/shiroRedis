ab0c07c870f6c16cb55b63aa79973766ca0860e4
zeyu.zhang <zeyu.zhang@ybm100.com>
2019-11-03 16:57:19 +0800

    新建项目上传

---
+++ src/main/java/com/zzyu/zzyushiroredis/ZzyushiroredisApplication.java
@@ -0,0 +1,13 @@
+package com.zzyu.zzyushiroredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzyushiroredisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzyushiroredisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzyu/zzyushiroredis/ZzyushiroredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzyu.zzyushiroredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzyushiroredisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
